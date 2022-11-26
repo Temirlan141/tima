d636982d1316c95d169831682c0035a7ab6cfb4a
Temirlan141 <114284125+Temirlan141@users.noreply.github.com>
2022-11-26 22:51:37 +0600

    Update NewToken.sol

--- workspaces/default_workspace/NewToken.sol
+++ workspaces/default_workspace/NewToken.sol
@@ -1,14 +1,79 @@
-pragma solidity ^0.8.7;
+// SPDX-License-Identifier: MIT
 
-import "./ERC20Standard.sol";
+pragma solidity ^0.8.0;
 
-contract NewToken is ERC20Standard {
-	constructor() public {
-		totalSupply = 100;
-		name = "Token by Alisher Ganiev";
-		decimals = 4;
-		symbol = "TA";
-		version = "1.0";
-		balances[msg.sender] = totalSupply;
-	}
+
+
+
+interface IERC20 {
+
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+
+
+contract MDR is IERC20 {
+    
+    string public constant name = "created by TIMA and SULTAN";
+    string public constant symbol = "BNB";
+    uint8 public constant decimals = 1;
+
+
+    mapping(address => uint256) balances;
+
+    mapping(address => mapping (address => uint256)) allowed;
+
+    uint256 totalSupply_ = 10000*(10*decimals);
+
+
+   constructor() {
+    balances[msg.sender] = totalSupply_;
+    }
+
+    function totalSupply() public override view returns (uint256) {
+    return totalSupply_;
+    }
+
+    function balanceOf(address tokenOwner) public override view returns (uint256) {
+        return balances[tokenOwner];
+    }
+
+    function transfer(address receiver, uint256 numTokens) public override returns (bool) {
+        require(numTokens <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender]-numTokens;
+        balances[receiver] = balances[receiver]+numTokens;
+        emit Transfer(msg.sender, receiver, numTokens);
+        return true;
+    }
+
+    function approve(address delegate, uint256 numTokens) public override returns (bool) {
+        allowed[msg.sender][delegate] = numTokens;
+        emit Approval(msg.sender, delegate, numTokens);
+        return true;
+    }
+
+    function allowance(address owner, address delegate) public override view returns (uint) {
+        return allowed[owner][delegate];
+    }
+
+    function transferFrom(address owner, address buyer, uint256 numTokens) public override returns (bool) {
+        require(numTokens <= balances[owner]);
+        require(numTokens <= allowed[owner][msg.sender]);
+
+        balances[owner] = balances[owner]-numTokens;
+        allowed[owner][msg.sender] = allowed[owner][msg.sender]-numTokens;
+        balances[buyer] = balances[buyer]+numTokens;
+        emit Transfer(owner, buyer, numTokens);
+        return true;
+    }
 }
